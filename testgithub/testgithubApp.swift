06a514d0343a9464db06463ce3c4cf8415996ac9
azamatzholoev <azamat.zholoev@gmail.com>
2022-09-24 08:21:30 +0900

    Initial Commit

---
+++ testgithub/testgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgithubApp.swift
+//  testgithub
+//
+//  Created by Azamat Dzholoev on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct testgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
